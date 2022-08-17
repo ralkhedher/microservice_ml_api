871ef1ef8a051240f912f4a138e53599eb8df1a5
Ralkhedher <Reemaalkhedher@gmail.com>
2022-08-17 13:14:04 +0000

    pep8

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask, request, jsonify
+from flask import (Flask, 
+                    request,
+                    jsonify)
 from flask.logging import create_logger
 import logging
 
